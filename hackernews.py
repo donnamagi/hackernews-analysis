9e3086f4ca204b7da46f7aafba8aac882abe7cd4
Donna Mägi <magi.donna@gmail.com>
2024-02-25 12:24:48 +0100

    cleanup

--- hackernews.py
+++ hackernews.py
@@ -1,10 +1,18 @@
 import requests
-import datetime
 
 # Hacker News API 
   
-def get_top_stories():
+def get_best_stories():
   url = 'https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty'
+
+  response = requests.get(url)
+  if response.status_code == 200:
+    return response.json()
+  else:
+    print(f"Error fetching data: {response.status_code}")
+
+def get_top_stories():
+  url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
 
   response = requests.get(url)
   if response.status_code == 200:
@@ -20,28 +28,3 @@ def get_hn_story(story_id):
     return response.json()
   else:
     print(f"Error fetching data: {response.status_code}")
-
-def get_stories(limit=20):
-  stories = dict()
-  top_stories = get_top_stories()
-  for story_id in top_stories[:limit]:
-    stories[story_id] = get_hn_story(story_id)
-  return stories
-
-def get_full_dict(stories):
-  data = []
-  for story in stories:
-    data.append({
-      'hn_id': story,
-      'title': stories[story]['title'],
-      'url': stories[story]['url'],
-      'hn_comment': stories[story]['text'] if 'text' in stories[story] else '',
-      'comment_ids': stories[story]['kids'] if 'kids' in stories[story] else [],
-      'date': datetime.datetime.now().strftime('%d-%m-%Y'),
-    })
-  return data
-
-def main():
-  stories = get_stories()
-  data = get_full_dict(stories)
-  return data
