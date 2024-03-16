eba4da75cd2d2218fdad47d53ef2b1da4e70806b
Donna Mägi <magi.donna@gmail.com>
2024-03-16 14:05:23 +0100

    cleanup + df export

--- df.py
+++ df.py
@@ -7,25 +7,42 @@ from datetime import datetime
 def get_all_keywords(df: pd.DataFrame):
   all_keywords = []
   for keywords_str in df['keywords']:
-    keywords = ast.literal_eval(keywords_str)
-    all_keywords.extend(keywords)
+    if type(keywords_str) == float: # NaN
+      continue
+    try:
+      keywords = ast.literal_eval(keywords_str)
+      all_keywords.extend(keywords)
+    except:
+      print(f"Error parsing: {keywords_str}")
   return all_keywords
 
 def count_keywords(keywords: list):
-  keywords_freq = Counter(keywords)
-  for keyword, freq in keywords_freq.most_common():
-      print(f'{keyword}: {freq}')
+  return Counter(keywords).most_common()
 
 def new_csv():
-  results = search_query(query= "id > 0", fields = ['id', 'keywords', 'date'], limit=1000)
+  results = search_query(query= "id > 0", fields = ['id', 'title', 'keywords'], limit=1000)
 
   df = pd.DataFrame(results)
 
   csv_file_path = f'export_{datetime.now().strftime("%Y-%m-%d")}.csv'
   df.to_csv(csv_file_path, index=False)
 
-def main():
-  df = pd.read_csv('export.csv')
-  count_keywords(get_all_keywords(df))
+def new_keywords_csv(keywords):
+  df = pd.DataFrame(keywords, columns=['keyword', 'frequency'])
+  csv_file_path = f'keywords_{datetime.now().strftime("%Y-%m-%d")}.csv'
+  df.to_csv(csv_file_path, index=False)
 
-new_csv()
+def main():
+  try:
+    df = pd.read_csv(f'export_{datetime.now().strftime("%Y-%m-%d")}.csv')
+  except FileNotFoundError:
+    new_csv()
+    df = pd.read_csv(f'export_{datetime.now().strftime("%Y-%m-%d")}.csv')
+
+  kw = count_keywords(get_all_keywords(df))
+  if input("Do you want to save the keywords in a csv file? (y/n): ") == 'y':
+    new_keywords_csv(kw) 
+  else:
+    print("Exiting.")
+
+main()
