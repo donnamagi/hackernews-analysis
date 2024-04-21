a82b073f2ad99fbad89b08d99493e1a70b482f81
Donna Mägi <magi.donna@gmail.com>
2024-04-21 15:38:11 +0200

    tabs with topics

--- streamlit.py
+++ streamlit.py
@@ -30,17 +30,53 @@ options = st.multiselect(
 st.scatter_chart(get_mentions_per_day(options), x='Date')
 
 
-keywords = ['Security', 'Backdoor', 'Open source']
-source = get_mentions_per_day(keywords)
-source['Total mentions'] = source[keywords].sum(axis=1)
 
-chart = alt.Chart(source).mark_circle().encode(
-    x='Date', y='Total mentions', color='Total mentions', tooltip=['Date', 'Security', 'Backdoor', 'Open source']
-).interactive()
+events = [
+  {
+    'title': 'XZ Backdoor attack discovered',
+    'annotations': [('Mar 29, 2024', 'XZ Backdoor attack discovered')],
+    'keywords': ['Security', 'Backdoor', 'Open source', 'XZ']
+  },
+  {
+    'title': 'Apple EU lawsuit',
+    'annotations': [('Mar 29, 2024', 'XZ Backdoor attack discovered')],
+    'keywords': ['Apple', 'EU', 'the European Union', 'Development']
+  },
+  {
+    'title': 'The AI Act',
+    'annotations': [('Mar 29, 2024', 'XZ Backdoor attack discovered')],
+    'keywords': ['EU', 'the European Union', 'Artificial intelligence', 'AI', 'Privacy']
+  }
+]
 
-tab1, tab2 = st.tabs(["XZ Backdoor attack", "tab2"])
+for event in events:
+  mentions_df = get_mentions_per_day(event['keywords'])
+  mentions_df['Total mentions'] = mentions_df[event['keywords']].sum(axis=1)
+
+  event['df'] = mentions_df
+
+st.write("## Events")
+
+titles = [event['title'] for event in events]
+tab1, tab2, tab3 = st.tabs(titles)
 
 with tab1:
-  st.altair_chart(chart, use_container_width=True)
+  st.write('A security backdoor was found in a widely used open source piece of software - XZ. The attack raised concerns about the security of open source software, and the execution sparked widespread discussion.')
+  xz_chart = alt.Chart(events[0]['df']).mark_circle().encode(
+    x='Date', y='Total mentions', color='Total mentions', tooltip=['Date'] + events[0]['keywords']
+  ).interactive()
+  st.altair_chart(xz_chart, use_container_width=True)
+
+
 with tab2:
-  st.altair_chart(chart, theme=None, use_container_width=True)
+  apple_chart = alt.Chart(events[1]['df']).mark_circle().encode(
+    x='Date', y='Total mentions', color='Total mentions', tooltip=['Date'] + events[1]['keywords']
+  ).interactive()
+  st.altair_chart(apple_chart, use_container_width=True)
+
+
+with tab3:
+  eu_chart = alt.Chart(events[2]['df']).mark_circle().encode(
+    x='Date', y='Total mentions', color='Total mentions', tooltip=['Date'] + events[2]['keywords']
+  ).interactive()
+  st.altair_chart(eu_chart, use_container_width=True)
